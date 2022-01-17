02b38f7be4fc9554e5c82005cfbc78d04aad35a6
aikoramdos <48702110+aikoramdos@users.noreply.github.com>
2022-01-17 12:17:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,10 @@ from discord.ext import commands
 from os import getenv
 import traceback
 
+import smtplib
+from email.message import EmailMessage
+
+
 bot = commands.Bot(command_prefix='/')
 
 
@@ -15,7 +19,23 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    # Open the plain text file whose name is in textfile for reading.
+    print "test!!"
+    msg = EmailMessage()
+    msg.set_content("test")
+
+    msg['Subject'] = 'test message'
+    msg['From'] = "test@example.jp"
+    msg['To'] = "65108" + "@" + "aikogakuen.net"
+
+    # Send the message via our own SMTP server.
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
+    
+    
 
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
+
